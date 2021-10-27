64755f75f04736e9e25c884a1c3967a72b28c5ec
CsignS <smileroom2010@gmail.com>
2021-10-27 15:23:33 +0700

    Initial commit and finish setting web security

---
+++ src/main/java/com/example/jwtphaipanda/JwtPhaipandaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtphaipanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPhaipandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtPhaipandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtphaipanda/JwtPhaipandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtphaipanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPhaipandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
